c7cae2056f43481a32ed22f6569930517bb033f7
manirsg <manirsg16@gmail.com>
2021-07-07 08:32:34 +0530

    New: Added Spinner

--- generators/createReact.js
+++ generators/createReact.js
@@ -1,11 +1,18 @@
+const ora = require('ora');
+const chalk = require('chalk');
+const { run } = require('./utils/helper');
 const createReactApp = answer => {
-    console.log(answer, 'create react');
+    // console.log(answer, 'create react');
+    let spinner = ora({ text: 'Creating react app...', spinner: "moon" }).start();
     run(`npx create-react-app ${answer.name}`, (err, stdOut, stdErr) => {
+
         if (err) {
-            console.error(err, 'error');
+            spinner.fail(chalk.red.bold('Error Creating React App'))
+            console.error(err);
             return;
         }
-        console.log('App created Successfully');
+        // console.log('App created Successfully');
+        spinner.stopAndPersist({ symbol: '🦄', text: chalk.green.bold('Happy Coding..!') });
     })
 }
 
--- generators/reactWithRedux.js
+++ generators/reactWithRedux.js
@@ -1,23 +1,34 @@
 const fs = require('fs');
 const fsPromises = require('fs/promises');
+const ora = require('ora');
+const chalk = require('chalk');
 const { SRC_ARR, ACTION_ARR, REDUCER_ARR } = require('./templates/reduxTemplates');
 const { run } = require('./utils/helper');
 
-const createTemplate = (path, filesArr) => {
+const createTemplate = (path, filesArr, spinnerText) => {
     return new Promise((resolve, reject) => {
+        let spinner = ora({text: `Creating ${spinnerText}`, spinner: "line"}).start();
+        
         fs.mkdir(path, { recursive: true }, error => {
             if (error) {
                 console.error(error, `unable to create ${path}`);
+                spinner.fail(chalk.redBright(`Error creating ${spinnerText}`));
                 //todo: resolve / reject
                 return;
             }
-            console.info(`${path} created`);
+            // console.info(`${path} created`);
+            spinner.succeed(chalk.greenBright(`${spinnerText} Created`));
 
             let writeArr = [];
             filesArr.forEach(file => {
+                let spinner = ora({ text: `Creating ${file.name}`, spinner: "line" }).start();
                 writeArr.push(fsPromises.writeFile(`${path}/${file.name}`, file.template).then(() => {
-                    console.info(`${file.name} created successfully`);
-                }, error => console.error(error, `Unable to create ${file.name}`)));
+                    // console.info(`${file.name} created successfully`);
+                    spinner.succeed(chalk.greenBright(`${spinnerText}/${file.name} Created`));
+                }, error => {
+                    spinner.fail(chalk.redBright(`Error creating ${spinnerText}/${file.name}`));
+                    console.error(error, `Unable to create ${file.name}`);
+                }));
             })
 
             Promise.all(writeArr).then(() => {
@@ -32,31 +43,36 @@ const createTemplate = (path, filesArr) => {
 const genReduxTemplate = answer => {
     let currentPath = process.cwd();
     answer.path = answer.path || 'src';
-    console.log(currentPath);
+    // console.log(currentPath); 
 
     const templateArr = [{
         path: `${currentPath}/${answer.name}/${answer.path}`,
-        filesArr: SRC_ARR
+        filesArr: SRC_ARR,
+        spinnerText: `/${answer.path}`
     }, {
         path: `${currentPath}/${answer.name}/${answer.path}/actions`,
-        filesArr: ACTION_ARR
+        filesArr: ACTION_ARR,
+        spinnerText: `/actions`
     }, {
         path: `${currentPath}/${answer.name}/${answer.path}/reducers`,
-        filesArr: REDUCER_ARR
+        filesArr: REDUCER_ARR,
+        spinnerText: `/reducers`
     }]
 
     let promiseArr = [];
 
     templateArr.forEach(template => {
-        promiseArr.push(createTemplate(template.path, template.filesArr).then(() => {
-            console.log(`${template.path} template created successfully`);
+
+        promiseArr.push(createTemplate(template.path, template.filesArr, template.spinnerText).then(() => {
+            // console.log(`${template.path} template created successfully`);
         }, error => {
             console.error(error, `Unable to create template ${template.path}`);
         }))
     })
 
     Promise.all(promiseArr).then(() => {
-        console.log("Created files successfully");
+        // console.log("Created files successfully");
+        ora(chalk.green.bold('Happy Coding..!')).stopAndPersist({ symbol: '🦄' });
     }, error => {
         console.error(error, `unable to create React with Redux template`);
     })
@@ -69,13 +85,18 @@ const createReduxTemplate = answer => {
               template: 'React with Redux'
           }
     */
-    console.log(answer, 'redux');
+    // console.log(answer, 'redux');
+    let spinner = ora({ text: 'Creating react app...', spinner: "moon" }).start();
+    // console.log(spinner)
     run(`npx create-react-app ${answer.name}`, (err, stdOut, stdErr) => {
         if (err) {
+            spinner.fail(chalk.red.bold('Error Creating React App'))
             console.error(err, 'error');
             return;
         }
-        // console.log(stdOut);
+
+        spinner.succeed(chalk.greenBright('React App Created'));
+
         genReduxTemplate(answer)
     })
 }
