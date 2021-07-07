d2f5feb95dcb871e283ab814ebe4d206f13db055
manirsg <manirsg16@gmail.com>
2021-07-07 20:58:43 +0530

    Delete: removed path parameter

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ program
     .command('create')
     .description('Generate react template')
     .requiredOption('-n, --name <appName>', 'React app name')
-    .option('-p, --path <path>', 'Path at which the template need to be created. By default templates will be created in src folder')
     .action(promptQn1)
 
 program.parse();
